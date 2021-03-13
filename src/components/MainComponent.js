5606af03c74cde5dad513294f8a6cdeace1bbd1a
Daniel Lavery <daniellavery6@gmail.com>
2021-03-13 00:13:06 -0500

    W3Complete

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -37,6 +37,16 @@ class Main extends Component {
             );
         };
 
+
+        const CampsiteWithId = ({match}) => {
+            return (
+                <CampsiteInfo 
+                    campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                    comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                />
+            );
+        };    
+
         return (
             <div>
                 <Header />
@@ -44,7 +54,9 @@ class Main extends Component {
                     <Route exact path='/contactus' component={Contact} />
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                     <Redirect to='/home' />
+                    
                 </Switch>
                 <Footer />
             </div>
