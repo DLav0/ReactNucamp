c2166ef1ef5f03f88dba54913a5e6bb44845064a
Daniel Lavery <lavery0@yahoo.com>
2021-03-22 23:12:41 -0400

    Combining Reducers

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem, Button, Modal, ModalHeader, ModalBody, Form, FormGroup, Label, Input} from 'reactstrap';
 import { Link } from 'react-router-dom';
-
+import { Control, LocalForm, Errors } from 'react-redux-form';
 
 
 function RenderCampsite({campsite}) {
@@ -23,18 +23,20 @@ console.log(comments)
 if (comments) {
     return (
         <div className='col-md-5 m-1'> 
-        <h4>Comments</h4>
-        {comments.map((comment) => {
-            return (
-                
-                <div className='row'>
-                    <p>{comment.text}<br/>
-                    -- {comment.author}, {' '}
-                    {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
-                </div>
-                
-            )})
-        }
+            <h4>Comments</h4>
+            {comments.map((comment) => {
+                return (
+                    
+                    <div className='row'>
+                        <p>{comment.text}<br/>
+                        -- {comment.author}, {' '}
+                        {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                    </div>
+                    
+                    
+                )})
+            }
+            <CommentForm />
         </div>
     )
 }  
@@ -61,13 +63,113 @@ function CampsiteInfo(props) {
                 <div className="row">
                     <RenderCampsite campsite={props.campsite} />
                     <RenderComments comments={props.comments} />
+              
                 </div>
+
             </div>
         );
     }
     return <div />;
 }
 
+class CommentForm extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {
+          
+          isModalOpen: false
+        };
+    }
+
+    toggleModal = () => {
+        this.setState({
+            isModalOpen:  !this.state.isModalOpen
+        });
+    }
+
+    handleSubmit = (values) => {
+        console.log(values);
+      };
+
+    
+    render() {
+        
+        return (
+
+
+       
+        <div>
+            <Button onClick={this.toggleModal} className="fa fa-pencil fa-lg" outline>
+             Submit Comment
+            </Button>
+            <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                <ModalHeader toggle={this.toggleModal}>
+                    Submit Comment
+                </ModalHeader>
+                <ModalBody>
+                    <LocalForm onSubmit={(valuesObj) => this.handleSubmit(valuesObj)}>
+                        <div className="form-group">
+                        <label htmlFor="contactType">
+                                Rating
+                        </label>
+                        <Control.select model=".contactType" id="contactType" name="contactType"
+                            className="form-control" defaultValue="1">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </Control.select>
+                        </div>
+                        
+                        <div className="form-group">
+                            <label htmlFor="name">
+                                Your Name
+                            </label>
+                            <Control.text 
+                            model=".name"
+                            id="name"
+                            name="name"
+                            className="form-control"
+                            validators={{
+                                required: (val) => val && val.length > 0,
+                                minLength: (val) => val && val.length > 2,
+                                maxLength: (val) => val && val.length <= 15
+                            }}
+                            />
+                            <Errors className="text-danger" model=".name" show="touched" component="div"
+                                messages={{
+                                    required: "REquired",
+                                    minLength: "Must be 2 letters",
+                                    maxLength: "must be less than 15 characters"
+                                }}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="comment">
+                                Comment
+                            </label>
+                            <Control.textarea
+                            model=".comment"
+                            id="comment"
+                            name="comment"
+                            className="form-control"
+                            rows="6"
+                            />
+
+                        </div>
+                        <Button type="submit" color="primary" onClick={this.toggleModal}>
+                            Submit
+                        </Button>
+                    </LocalForm>
+                </ModalBody>
+            </Modal>
+
+        </div>
+
+        )
+    }
+}
 
 export default CampsiteInfo
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,3 +1,5 @@
+
+
 import { CAMPSITES } from '../shared/campsites';
 import { COMMENTS } from '../shared/comments';
 import { PARTNERS } from '../shared/partners';
