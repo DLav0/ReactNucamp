acf1570846a3d64c698b070906a1f6929f0ca834
Daniel Lavery <lavery0@yahoo.com>
2021-03-26 22:58:34 -0400

    Fetch Post Comment

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -20,7 +20,7 @@ function RenderCampsite({campsite}) {
     )
 }
 
-function RenderComments({comments, addComment, campsiteId}) {
+function RenderComments({comments, postComment, campsiteId}) {
 
 // console.log(comments)
 
@@ -40,7 +40,7 @@ if (comments) {
                     
                 )})
             }
-            <CommentForm campsiteId={campsiteId} addComment={addComment}/>
+            <CommentForm campsiteId={campsiteId} postComment={postComment}/>
         </div>
     )
 }  
@@ -88,7 +88,7 @@ function CampsiteInfo(props) {
                     <RenderCampsite campsite={props.campsite} />
                     <RenderComments 
                         comments={props.comments} 
-                        addComment={props.addComment}
+                        postComment={props.postComment}
                         campsiteId={props.campsite.id}
                     />
               
@@ -118,7 +118,7 @@ class CommentForm extends Component {
 
     handleSubmit = (values) => {
         this.toggleModal();
-        this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+        this.props.postComment(this.props.campsiteId, values.rating, values.author, values.text);
       };
 
     
